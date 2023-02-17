726e727eb02d4cc86a87912305d7054aaeac2cb7
tsujinho3 <29732798+tsujinho3@users.noreply.github.com>
2023-02-18 01:30:12 +0900

    feat: add "items" end point

--- src/main.py
+++ src/main.py
@@ -5,4 +5,9 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hellp Wprld"}
+    return {"message": "Hello Wprld"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
